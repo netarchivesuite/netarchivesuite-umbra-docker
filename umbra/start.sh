3247c3e116dfc0cdc6cb35b2483711d9764e03bb
Colin Rosenthal <csr@statsbiblioteket.dk>
2018-09-28 10:41:40 +0200

    Elastic beanstalk version of this docker service.

--- umbra/start.sh
+++ umbra/start.sh
@@ -1,3 +1,3 @@
 ulimit -c 0
 drain-queue --url amqp://guest:guest@activemq:5672/%2f
-umbra -n 5 --executable /usr/bin/run-chromium  --url amqp://guest:guest@activemq:5672/%2f
+umbra -n 1 --executable /usr/bin/run-chromium  --url amqp://guest:guest@activemq:5672/%2fumbra 
